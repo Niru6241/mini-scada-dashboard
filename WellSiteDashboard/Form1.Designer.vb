df8b9a7e275cb6d9dd4361b1d2638857b6b0c871
Niranjan <niru18.gitam@gmail.com>
2025-08-18 17:54:58 -0600

    Initial commit - Mini SCADA/HMI Dashboard

---
+++ WellSiteDashboard/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend3 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series5 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim Series6 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.lblSensorValue = New System.Windows.Forms.Label()
+        Me.lblAlert = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblSensorValue2 = New System.Windows.Forms.Label()
+        Me.lblAlert2 = New System.Windows.Forms.Label()
+        Me.chartSensors = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.btnStop = New System.Windows.Forms.Button()
+        CType(Me.chartSensors, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblSensorValue
+        '
+        Me.lblSensorValue.AutoSize = True
+        Me.lblSensorValue.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lblSensorValue.Location = New System.Drawing.Point(133, 93)
+        Me.lblSensorValue.Name = "lblSensorValue"
+        Me.lblSensorValue.Size = New System.Drawing.Size(100, 36)
+        Me.lblSensorValue.TabIndex = 0
+        Me.lblSensorValue.Text = "Well1:"
+        '
+        'lblAlert
+        '
+        Me.lblAlert.AutoSize = True
+        Me.lblAlert.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lblAlert.ForeColor = System.Drawing.Color.Red
+        Me.lblAlert.Location = New System.Drawing.Point(144, 150)
+        Me.lblAlert.Name = "lblAlert"
+        Me.lblAlert.Size = New System.Drawing.Size(68, 29)
+        Me.lblAlert.TabIndex = 1
+        Me.lblAlert.Text = "Alert:"
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(158, 247)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 2
+        Me.btnStart.Text = "Start Simulation"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'lblSensorValue2
+        '
+        Me.lblSensorValue2.AutoSize = True
+        Me.lblSensorValue2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lblSensorValue2.Location = New System.Drawing.Point(569, 93)
+        Me.lblSensorValue2.Name = "lblSensorValue2"
+        Me.lblSensorValue2.Size = New System.Drawing.Size(80, 29)
+        Me.lblSensorValue2.TabIndex = 3
+        Me.lblSensorValue2.Text = "Well2:"
+        '
+        'lblAlert2
+        '
+        Me.lblAlert2.AutoSize = True
+        Me.lblAlert2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lblAlert2.ForeColor = System.Drawing.Color.Red
+        Me.lblAlert2.Location = New System.Drawing.Point(568, 150)
+        Me.lblAlert2.Name = "lblAlert2"
+        Me.lblAlert2.Size = New System.Drawing.Size(68, 29)
+        Me.lblAlert2.TabIndex = 4
+        Me.lblAlert2.Text = "Alert:"
+        '
+        'chartSensors
+        '
+        ChartArea3.Name = "ChartArea1"
+        Me.chartSensors.ChartAreas.Add(ChartArea3)
+        Legend3.Name = "Legend1"
+        Me.chartSensors.Legends.Add(Legend3)
+        Me.chartSensors.Location = New System.Drawing.Point(499, 247)
+        Me.chartSensors.Name = "chartSensors"
+        Series5.ChartArea = "ChartArea1"
+        Series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series5.Legend = "Legend1"
+        Series5.Name = "Well1"
+        Series6.ChartArea = "ChartArea1"
+        Series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series6.Legend = "Legend1"
+        Series6.Name = "Well2"
+        Me.chartSensors.Series.Add(Series5)
+        Me.chartSensors.Series.Add(Series6)
+        Me.chartSensors.Size = New System.Drawing.Size(387, 265)
+        Me.chartSensors.TabIndex = 5
+        Me.chartSensors.Text = "Chart1"
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(322, 247)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 6
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(937, 524)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.chartSensors)
+        Me.Controls.Add(Me.lblAlert2)
+        Me.Controls.Add(Me.lblSensorValue2)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblAlert)
+        Me.Controls.Add(Me.lblSensorValue)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.chartSensors, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblSensorValue As Label
+    Friend WithEvents lblAlert As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblSensorValue2 As Label
+    Friend WithEvents lblAlert2 As Label
+    Friend WithEvents chartSensors As DataVisualization.Charting.Chart
+    Friend WithEvents btnStop As Button
+End Class
---
+++ WellSiteDashboard/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports Npgsql
+Imports System.Timers
+
+Public Class Form1
+    Dim rnd As New Random()
+    Dim WithEvents sensorTimer As New System.Windows.Forms.Timer()
+    Dim dbPassword As String = Environment.GetEnvironmentVariable("PGPASSWORD")
+    Dim connStr As String = "Host=localhost;Port=5432;Username=postgres;Password={dbPassword};Database=wellsite_db"
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        sensorTimer.Interval = 2000 ' 2 seconds
+        sensorTimer.Start()
+        btnStart.Enabled = False
+    End Sub
+
+    Private Sub sensorTimer_Tick(sender As Object, e As EventArgs) Handles sensorTimer.Tick
+        ' Generate random sensor value
+        Dim sensorValue As Double = rnd.NextDouble() * 100
+        lblSensorValue.Text = "Well1: " & Math.Round(sensorValue, 2).ToString()
+
+        ' Show alert if value > 80
+        If sensorValue > 80 Then
+            lblAlert.ForeColor = Color.Red
+            lblAlert.Text = "Critical Value!"
+        ElseIf sensorValue > 60 Then
+            lblAlert.ForeColor = Color.Orange
+            lblAlert.Text = "Warning Value"
+        Else
+            lblAlert.ForeColor = Color.Black
+            lblAlert.Text = ""
+        End If
+
+        ' --- Well2 (New) ---
+        Dim sensorValue2 As Double = rnd.NextDouble() * 100
+        lblSensorValue2.Text = "Well2: " & Math.Round(sensorValue2, 2).ToString()
+        If sensorValue2 > 80 Then
+            lblAlert2.ForeColor = Color.Red
+            lblAlert2.Text = "Critical Value!"
+        ElseIf sensorValue2 > 60 Then
+            lblAlert2.ForeColor = Color.Orange
+            lblAlert2.Text = "Warning Value"
+        Else
+            lblAlert2.ForeColor = Color.Black
+            lblAlert2.Text = ""
+        End If
+
+        ' Add points to chart
+        chartSensors.Series("Well1").Points.AddY(sensorValue)
+        chartSensors.Series("Well2").Points.AddY(sensorValue2)
+
+        ' Optional: keep only last 20 points to avoid overcrowding
+        If chartSensors.Series("Well1").Points.Count > 20 Then
+            chartSensors.Series("Well1").Points.RemoveAt(0)
+            chartSensors.Series("Well2").Points.RemoveAt(0)
+        End If
+
+        ' Insert into PostgreSQL
+        Using conn As New NpgsqlConnection(connStr)
+            Try
+                conn.Open()
+                Dim sql As String = "INSERT INTO sensor_readings (sensor_name, value) VALUES (@sensorName, @value)"
+                Using cmd As New NpgsqlCommand(sql, conn)
+                    cmd.Parameters.AddWithValue("@sensorName", "Well1")
+                    cmd.Parameters.AddWithValue("@value", sensorValue)
+                    cmd.ExecuteNonQuery()
+                End Using
+
+                Using cmd2 As New NpgsqlCommand(sql, conn)
+                    cmd2.Parameters.AddWithValue("@sensorName", "Well2")
+                    cmd2.Parameters.AddWithValue("@value", sensorValue2)
+                    cmd2.ExecuteNonQuery()
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("DB Error: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub chartSensors_Click(sender As Object, e As EventArgs) Handles chartSensors.Click
+
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        sensorTimer.Stop()                ' Stop the timer
+        btnStart.Enabled = True           ' Re-enable the Start button
+        lblAlert.Text = "Simulation Stopped"
+        lblAlert2.Text = "Simulation Stopped"
+    End Sub
+End Class
